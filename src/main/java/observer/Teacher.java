af46adf11aef8493a84bddd4c56013b0dea0b20d
lengdidi <634930228@qq.com>
2019-06-15 19:08:02 +0800

    shejimoshi

--- src/main/java/observer/Teacher.java
+++ src/main/java/observer/Teacher.java
@@ -14,6 +14,7 @@ public class Teacher implements Observer {
     public void update(Observable o, Object arg) {
         Course course = (Course) o;
         Question question = (Question) arg;
-        System.out.println(teacherName + "老师的" + course.getCourseName() + "课程接收到一个" + question.getUserName() + "提交的问答" + question.getQuestionContent());
+        System.out.println(teacherName + "老师的" + course.getCourseName() + "课程接收到一个"
+                + question.getUserName() + "提交的问答" + question.getQuestionContent());
     }
 }
